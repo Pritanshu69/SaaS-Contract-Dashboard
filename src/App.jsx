b9a4864ac6c559f1f18333f6c184a67aec87eb41
Pritanshu69 <samantaprit1924@gmail.com>
2025-09-14 17:53:01 +0530

    setup

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-      <h1 className="text-4xl font-bold text-white">🚀 Tailwind v4.1 is working!</h1>
-    </div>
-  )
-}
